51c068e4911b83661935682abd0da8df40769c5f
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-23 12:01:57 +0530

    Learnt about Interfaces and Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 import Input from './components/Input'
+import { Todo } from './model'
 
 //Types in Typescript 
 
 
 const App : React.FC = () =>{
+
+  const [todo,setTodo] = useState<string>("")
+
+  const [myTodos,setMyTodos] = useState<Todo[]>([]); //creating an array of interface;
+
+
+
+  const handleAdd=(e:React.FormEvent)=>{
+      e.preventDefault();
+
+      if(myTodos){
+        setMyTodos([...myTodos,{id:Date.now(),todo:todo,isDone:false}]);
+        setTodo("");
+      }
+  }
+
+  console.log(myTodos);
+
   return (
     <div className='App'>
           <h1 className='heading'>Taskify</h1>
-          <Input/>
+          <Input todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+          <TodoList/>
     </div>
   )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,29 @@
+import { useRef } from 'react';
 import './styles.css'
 
-const Input :React.FC = () => {
-  return (
-    <form className="input">
-        <input type="input" placeholder="Enter a task" className="input_box"/>
-        <button className="input_submit" type="submit">Go</button>
-    </form>
-  )
+
+interface Props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>,
+    handleAdd: (e: React.FormEvent) => void;  //because this function return nothing
+}
+
+const Input = ({ todo, setTodo, handleAdd }: Props) => {
+
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+
+    return (
+        <form className="input" onSubmit={(e) => {
+            handleAdd(e)
+            inputRef.current?.blur()
+        }}
+        >
+            <input ref={inputRef} value={todo} onChange={(e) => setTodo(e.target.value)} type="input" placeholder="Enter a task" className="input_box" />
+            <button className="input_submit" type="submit">Go</button>
+        </form>
+    )
 }
 
 export default Input
